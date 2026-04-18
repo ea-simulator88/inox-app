afa0db2bae5139e679709a90995fe00ff9860977
ea-simulator88 <zhangweiquan91@gmail.com>
2026-04-18 10:18:35 +0700

    update app

--- update.bat
+++ update.bat
@@ -1,10 +1,8 @@
 @echo off
-chcp 65001 >nul
-pushd "D:\Excel\Work\Python\App xuất nhập hàng inox\Xuất nhập hàng"
+cd /d "D:\Excel\Work\Python\App xuất nhập hàng inox\Xuất nhập hàng"
 git add .
 git commit -m "update app"
 git push origin main
-popd
 echo.
 echo === DONE! App da duoc cap nhat ===
 pause
