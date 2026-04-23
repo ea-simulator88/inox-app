f041370878121d45d77c79f25daa9311deb78058
ea-simulator88 <zhangweiquan91@gmail.com>
2026-04-23 13:25:48 +0700

    update app

--- update.bat
+++ update.bat
@@ -1,43 +1,7 @@
 @echo off
-
-REM === THU MUC ===
-set SOURCE_DIR=D:\Excel\Work\Python\App xuất nhập hàng inox\Xuất nhập hàng
-set BACKUP_DIR=D:\Excel\Work\Python\App xuất nhập hàng inox\Backup
-
-cd /d "%SOURCE_DIR%"
-
-REM === TIM SO MAX TU FILE INDEX ===
-set max=0
-
-for %%f in ("%BACKUP_DIR%\index *.html") do (
-    set "name=%%~nf"
-    setlocal enabledelayedexpansion
-    set "num=!name:index =!"
-    if !num! GTR !max! (
-        endlocal & set max=!num!
-    ) else (
-        endlocal
-    )
-)
-
-REM === SO MOI ===
-set /a next=%max%+1
-
-REM === BACKUP HTML ===
-copy "%SOURCE_DIR%\index.html" "%BACKUP_DIR%\index %next%.html"
-
-REM === BACKUP JS (TEN FILE MOI) ===
-copy "%SOURCE_DIR%\App Script.js" "%BACKUP_DIR%\App Script %next%.js"
-
-echo Backup created:
-echo - index %next%.html
-echo - App Script %next%.js
-
-REM === GIT ===
+cd /d "D:\Excel\Work\Python\App xuất nhập hàng inox\Xuất nhập hàng"
 git add .
 git commit -m "update app"
 git push origin main
-
 echo.
 echo === DONE! App da duoc cap nhat ===
-pause
